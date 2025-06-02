51bcafc8190e756d9ce08fa5ab0c20fb6803ae47
pratimaasapkota <pratimasapkota509@gmail.com>
2025-06-01 21:21:28 -1100

    wrap up today

--- day1/script.js
+++ day1/script.js
@@ -1,3 +1,54 @@
+//callbacks and callbackhell
+function getData(callback) {
+  setTimeout(() => {
+    console.log("Data received");
+    callback();
+  }, 1000);
+}
+
+getData(() => {
+  console.log("Callback executed");
+});
+ setTimeout(() => {
+  console.log("Task 1");
+  setTimeout(() => {
+    console.log("Task 2");
+    setTimeout(() => {
+      console.log("Task 3");
+    }, 1000);
+  }, 1000);
+}, 1000);
+
+//promises and promise chaining
+const getData = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Data received");
+    }, 1000);
+  });
+};
+
+getData().then(data => {
+  console.log(data);
+});
+const fetchData = () => {
+  return new Promise(resolve => {
+    setTimeout(() => resolve("Step 1"), 1000);
+  });
+};
+
+fetchData()
+  .then(result => {
+    console.log(result);
+    return "Step 2";
+  })
+  .then(step => {
+    console.log(step);
+    return "Step 3";
+  })
+  .then(console.log);
+
+
 async function createPost() {
   const post = {
     title: "Hello World",
