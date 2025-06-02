82b560226217191eecb363c70aae410042725b3c
pratimaasapkota <pratimasapkota509@gmail.com>
2025-06-01 19:46:55 -1100

    fetch api

--- day1/script.js
+++ day1/script.js
@@ -1,21 +1,24 @@
-//Error handeling in promises
-const riskyOperation = () => {
-  return new Promise((resolve, reject) => {
-    const success = Math.random() > 0.5;
-    setTimeout(() => {
-      success ? resolve("Success") : reject("Failure");
-    }, 1000);
-  });
-};
+async function createPost() {
+  const post = {
+    title: "Hello World",
+    body: "This is my first post.",
+    userId: 1
+  };
 
-riskyOperation()
-  .then(console.log)
-  .catch(error => {
-    console.error("Caught error:", error);
-  });
-Promise.all([
-  Promise.resolve("First"),
-  new Promise(res => setTimeout(() => res("Second"), 1000)),
-  Promise.resolve("Third")
-]).then(console.log);
- 
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(post)
+    });
+
+    const data = await response.json();
+    console.log("Created Post:", data);
+  } catch (error) {
+    console.error("Error creating post:", error);
+  }
+}
+
+createPost();
