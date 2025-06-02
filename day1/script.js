945b0433cb698aab6e1cbcbda7866151be77b1bf
pratimaasapkota <pratimasapkota509@gmail.com>
2025-06-01 19:16:37 -1100

    promises

--- day1/script.js
+++ day1/script.js
@@ -1,23 +1,3 @@
-//callback and callback held 
-function getData(callback) {
-  setTimeout(() => {
-    console.log("Data received");
-    callback();
-  }, 1000);
-}
-
-getData(() => {
-  console.log("Callback executed");
-});
-setTimeout(() => {
-  console.log("Task 1");
-  setTimeout(() => {
-    console.log("Task 2");
-    setTimeout(() => {
-      console.log("Task 3");
-    }, 1000);
-  }, 1000);
-}, 1000);
 //promises
 const getData = () => {
   return new Promise((resolve, reject) => {
@@ -30,7 +10,7 @@ const getData = () => {
 getData().then(data => {
   console.log(data);
 });
-//promise chainning
+//promise chaining
 const fetchData = () => {
   return new Promise(resolve => {
     setTimeout(() => resolve("Step 1"), 1000);
@@ -47,5 +27,3 @@ fetchData()
     return "Step 3";
   })
   .then(console.log);
-
- 
