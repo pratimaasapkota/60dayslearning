39be2f1cf82e0824b6b09e8c450efcf1781af595
pratimaasapkota <pratimasapkota509@gmail.com>
2025-06-01 13:59:06 -1100

    string print

--- day1/index.js
+++ day1/index.js
@@ -1,8 +1,20 @@
-console.log("i am a tutorial on loops")
-let a=1;
-console.log(a);
+// console.log("i am a tutorial on loops")
+// let a=1;
+// console.log(a);
 
-for(let i=0; i<100; i++)
-{
-    console.log(a+i);
-}
+// for(let i=0; i<100; i++)
+// {
+//     console.log(a+i);
+// }
+// let i=20;
+// do{
+//     console.log(i)
+//     i++;
+    
+
+// }while(i<6);
+let a="pratima";
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])               
+    
