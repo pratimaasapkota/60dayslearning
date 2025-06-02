a57d9c0b617373952c168054023ee976a6e41cab
pratimaasapkota <pratimasapkota509@gmail.com>
2025-06-01 14:19:11 -1100

    array learning

--- day1/index.js
+++ day1/index.js
@@ -1,20 +1,5 @@
-// console.log("i am a tutorial on loops")
-// let a=1;
-// console.log(a);
-
-// for(let i=0; i<100; i++)
-// {
-//     console.log(a+i);
-// }
-// let i=20;
-// do{
-//     console.log(i)
-//     i++;
-    
-
-// }while(i<6);
-let a="pratima";
-console.log(a[0])
-console.log(a[1])
-console.log(a[2])               
-    
+let arr=[1, 2, 3, 4, 5]
+console.log(arr);
+console.log(arr.length);
+console.log(arr[0]);
+console.log(arr[1]);    
