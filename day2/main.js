912b7d450e127a90fb78c6383d81089ef2a4a695
pratimaasapkota <pratimasapkota509@gmail.com>
2025-06-02 14:17:37 -1100

    server

--- day2/main.js
+++ day2/main.js
@@ -2,5 +2,12 @@ const fs =require('fs')
 
 console.log(fs) 
 console.log("starting")
-fs.writeFileSync("pratima.txt", "pratima is a good girl")
+// fs.writeFileSync("pratima.txt", "pratima is a good girl")
+fs.writeFile("pratima2.txt", "pratima is a good girl",()=>{
+    console.log("done")
+    fs.readFile("pratima2.txt", (error, data) => {
+        console.log(error,data.toString())
+})
+})
 console.log("ending")
+
