d9abb5422b0af0130a59bd00bfe71072c14d5a21
pratimaasapkota <pratimasapkota509@gmail.com>
2025-06-08 12:47:15 -1100

    counter app

---
+++ day8/my-app/src/context/ThemeContext.jsx
@@ -0,0 +1,19 @@
+import React, { createContext, useState } from 'react';
+
+// Create Context
+export const ThemeContext = createContext();
+
+// Provider Component
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () => {
+    setTheme((prev) => (prev === 'light' ? 'dark' : 'light'));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
