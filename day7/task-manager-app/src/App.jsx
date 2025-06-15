011a081e3894ecae582cdfad3860265e6c326a39
pratimaasapkota <pratimasapkota509@gmail.com>
2025-06-14 14:47:23 -1100

    task manager

--- day7/task-manager-app/src/App.jsx
+++ day7/task-manager-app/src/App.jsx
@@ -4,7 +4,12 @@ import TaskList from './components/TaskList';
 
 function App() {
   return (
-    
+    <TaskProvider>
+      <div className="max-w-md mx-auto mt-10 p-6 border rounded shadow">
+        <h1 className="text-2xl mb-4 text-center font-bold">📝 Task Manager</h1>
+        <TaskForm />
+        <TaskList />
+      </div>
     </TaskProvider>
   );
 }
